bf9dc190ce465f1bd0cbda5c58120225be11aabe
Nicholas Jamieson <nicholas@cartant.com>
2018-03-29 13:51:00 +1000

    chore(knowns): Lookup pipeable operators.

--- source/support/knowns.ts
+++ source/support/knowns.ts
@@ -13,38 +13,41 @@ let dist: string | undefined = undefined;
 let prototypeMethods: { [key: string]: boolean } = {};
 
 try {
-
     const entry = resolve.sync("rxjs");
     dist = path.dirname(entry);
-
     const root = dist.replace(/node_modules[\/\\]rxjs[\/\\](.*)$/, (match) => match);
     const { version } = require(path.join(root, "package.json"));
     prototypeMethods = semver.satisfies(version, "<5.5.0-beta.5") ? {} : { pipe: true, toPromise: true };
-
 } catch (error) {
-    /*tslint:disable-next-line:no-console*/
-    console.warn("Cannot find rxjs in node_modules; some rxjs-tslint-rules will be ineffectual.");
+    warn();
 }
 
 function read(dir: string): { [key: string]: string } {
-
     if (!dist) {
         return {};
     }
-
-    // In RxJS 5.5.0-beta.5, toPromise was moved into the prototype.
-    // However, to avoid breakage, the file in the add/operator directory was
-    // left in-place.
-
-    const names = fs.readdirSync(path.join(dist, dir));
-    return names
-        .filter((name) => /^[a-z]\w+\.js$/.test(name))
-        .map((name) => name.replace(/\.js/, ""))
-        .filter((name) => !prototypeMethods[name])
-        .reduce((acc, name) => ({ ...acc, [name]: name }), {});
+    try {
+        // In RxJS 5.5.0-beta.5, toPromise was moved into the prototype.
+        // However, to avoid breakage, the file in the add/operator directory was
+        // left in-place.
+        const names = fs.readdirSync(path.join(dist, dir));
+        return names
+            .filter((name) => /^[a-z]\w+\.js$/.test(name))
+            .map((name) => name.replace(/\.js/, ""))
+            .filter((name) => !prototypeMethods[name])
+            .reduce((acc, name) => ({ ...acc, [name]: name }), {});
+    } catch (error) {
+        warn(dir);
+    }
 }
 
 export const knownObservables = read("add/observable");
 export const knownOperators = { ...read("add/operator"), ...aliasOperators };
+export const knownPipeableOperators = { ...read("operators"), ...aliasOperators };
 export const knownPrototypeMethods = { forEach: true, ...prototypeMethods };
 export const knownStaticMethods = { create: true };
+
+function warn(dir: string = ""): void {
+    /*tslint:disable-next-line:no-console*/
+    console.warn(`Cannot find node_modules/rxjs/${dir}; some rxjs-tslint-rules will be ineffectual.`);
+}
