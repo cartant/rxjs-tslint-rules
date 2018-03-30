30901b9e9daeb9a19816145f645bb1deb539ba1e
Nicholas Jamieson <nicholas@cartant.com>
2018-03-30 11:39:28 +1000

    chore(scope): Improve rule description.

--- source/rules/rxjsNoUnsafeScopeRule.ts
+++ source/rules/rxjsNoUnsafeScopeRule.ts
@@ -13,7 +13,7 @@ import { couldBeType } from "../support/util";
 export class Rule extends Lint.Rules.TypedRule {
 
     public static metadata: Lint.IRuleMetadata = {
-        description: "Disallows unsafe scopes.",
+        description: "Disallows the use of variables/properties from unsafe/outer scopes in operator callbacks.",
         options: null,
         optionsDescription: "Not configurable.",
         requiresTypeInfo: true,
