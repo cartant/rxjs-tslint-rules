3826de76440543c6b0f93757b77b32fae7e38c3b
Nicholas Jamieson <nicholas@cartant.com>
2018-05-14 14:39:43 +1000

    test(tslint): Add more TSLint-based fixtures.

---
+++ test/v5/fixtures/ban-observables/patched-elsewhere/add.ts
@@ -0,0 +1,4 @@
+/*tslint:disable*/
+import "rxjs/add/observable/concat";
+import "rxjs/add/observable/of";
+import "rxjs/add/observable/merge";
---
+++ test/v6-compat/fixtures/ban-operators/patched-elsewhere/add.ts
@@ -0,0 +1,4 @@
+/*tslint:disable*/
+import "rxjs/add/observable/of";
+import "rxjs/add/operator/concat";
+import "rxjs/add/operator/merge";
