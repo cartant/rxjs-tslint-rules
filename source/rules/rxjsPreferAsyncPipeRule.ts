b25af740bb3cfd3353dbdb41d1971407baa4cabb
Nicholas Jamieson <nicholas@cartant.com>
2018-10-06 11:51:51 +1000

    feat(prefer-async-pipe): Add rule.

---
+++ source/rules/rxjsPreferAsyncPipeRule.ts
@@ -0,0 +1,59 @@
+/**
+ * @license Use of this source code is governed by an MIT-style license that
+ * can be found in the LICENSE file at https://github.com/cartant/rxjs-tslint-rules
+ */
+
+import * as Lint from "tslint";
+import * as ts from "typescript";
+import * as tsutils from "tsutils";
+import { tsquery } from "@phenomnomnominal/tsquery";
+import { couldBeType } from "../support/util";
+
+export class Rule extends Lint.Rules.TypedRule {
+
+    public static metadata: Lint.IRuleMetadata = {
+        description: "Disallows the calling of `subscribe` within an Angular component.",
+        options: null,
+        optionsDescription: "Not configurable.",
+        requiresTypeInfo: true,
+        ruleName: "rxjs-prefer-async-pipe",
+        type: "style",
+        typescriptOnly: true
+    };
+
+    public static FAILURE_STRING = "Prefer async pipe over subscribe";
+
+    public applyWithProgram(sourceFile: ts.SourceFile, program: ts.Program): Lint.RuleFailure[] {
+
+        const failures: Lint.RuleFailure[] = [];
+        const typeChecker = program.getTypeChecker();
+
+        const componentIdentifiers = tsquery(
+            sourceFile,
+            `ClassDeclaration Identifier[name=/Component$/]`
+        );
+        componentIdentifiers.forEach(componentIdentifier => {
+            const { parent: classDeclaration } = componentIdentifier;
+            const propertyAccessExpressions = tsquery(
+                classDeclaration,
+                `CallExpression PropertyAccessExpression[name.name="subscribe"]`
+            );
+            propertyAccessExpressions.forEach(propertyAccessExpression => {
+                if (tsutils.isPropertyAccessExpression(propertyAccessExpression)) {
+                    const type = typeChecker.getTypeAtLocation(propertyAccessExpression.expression);
+                    if (couldBeType(type, "Observable")) {
+                        const { name } = propertyAccessExpression;
+                        failures.push(new Lint.RuleFailure(
+                            sourceFile,
+                            name.getStart(),
+                            name.getStart() + name.getWidth(),
+                            Rule.FAILURE_STRING,
+                            this.ruleName
+                        ));
+                    }
+                }
+            });
+        });
+        return failures;
+    }
+}
