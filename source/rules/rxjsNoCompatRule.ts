ae4403f5e5f56aacf1882755e5be9135d32917e9
Nicholas Jamieson <nicholas@cartant.com>
2019-01-18 12:23:22 +1000

    feat(no-compat): Add no-compat rule.

---
+++ source/rules/rxjsNoCompatRule.ts
@@ -0,0 +1,47 @@
+/**
+ * @license Use of this source code is governed by an MIT-style license that
+ * can be found in the LICENSE file at https://github.com/cartant/rxjs-tslint-rules
+ */
+/*tslint:disable:no-use-before-declare*/
+
+import * as Lint from "tslint";
+import * as ts from "typescript";
+import { tsquery } from "@phenomnomnominal/tsquery";
+
+export class Rule extends Lint.Rules.AbstractRule {
+
+    public static metadata: Lint.IRuleMetadata = {
+        deprecationMessage: undefined,
+        description: "Disallows importation from locations that depend upon 'rxjs-compat'.",
+        options: null,
+        optionsDescription: "Not configurable.",
+        requiresTypeInfo: false,
+        ruleName: "rxjs-no-compat",
+        type: "functionality",
+        typescriptOnly: false
+    };
+
+    public static FAILURE_STRING = "'rxjs-compat' dependent import locations are forbidden";
+
+    public apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
+        const failures: Lint.RuleFailure[] = [];
+        const importDeclarations = tsquery(
+            sourceFile,
+            `ImportDeclaration:has(StringLiteral[value=/^rxjs/])`
+        );
+        importDeclarations.forEach(node => {
+            const importDeclaration = node as ts.ImportDeclaration;
+            const { moduleSpecifier } = importDeclaration;
+            if (!/^['"]rxjs(\/(ajax|operators|testing|webSocket))?['"]$/.test(moduleSpecifier.getText())) {
+                failures.push(new Lint.RuleFailure(
+                    sourceFile,
+                    moduleSpecifier.getStart(),
+                    moduleSpecifier.getStart() + moduleSpecifier.getWidth(),
+                    Rule.FAILURE_STRING,
+                    this.ruleName
+                ));
+            }
+        });
+        return failures;
+    }
+}
