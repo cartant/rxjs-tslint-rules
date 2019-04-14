c5a9130da1b0f5db9203ff26e4f4718b8fafb709
Nicholas Jamieson <nicholas@cartant.com>
2019-04-14 14:31:49 +1000

    chore: Remove unnecessary lint flag.

--- source/rules/rxjsNoExplicitGenericsRule.ts
+++ source/rules/rxjsNoExplicitGenericsRule.ts
@@ -2,7 +2,6 @@
  * @license Use of this source code is governed by an MIT-style license that
  * can be found in the LICENSE file at https://github.com/cartant/rxjs-tslint-rules
  */
-/*tslint:disable:no-use-before-declare*/
 
 import * as Lint from "tslint";
 import * as ts from "typescript";
--- source/rules/rxjsNoIgnoredObservableRule.ts
+++ source/rules/rxjsNoIgnoredObservableRule.ts
@@ -2,7 +2,6 @@
  * @license Use of this source code is governed by an MIT-style license that
  * can be found in the LICENSE file at https://github.com/cartant/rxjs-tslint-rules
  */
-/*tslint:disable:no-use-before-declare*/
 
 import { tsquery } from "@phenomnomnominal/tsquery";
 import * as Lint from "tslint";
