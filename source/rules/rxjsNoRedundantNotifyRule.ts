c08c9190aa6059d81ea335d6fdab773ec2b2ab8e
Nicholas Jamieson <nicholas@cartant.com>
2019-11-15 13:35:43 +1000

    fix: Fix no-redundant-notify false positives.

--- source/rules/rxjsNoRedundantNotifyRule.ts
+++ source/rules/rxjsNoRedundantNotifyRule.ts
@@ -6,6 +6,7 @@
 import { tsquery } from "@phenomnomnominal/tsquery";
 import * as Lint from "tslint";
 import * as ts from "typescript";
+import { couldBeType } from "../support/util";
 
 export class Rule extends Lint.Rules.TypedRule {
   public static metadata: Lint.IRuleMetadata = {
@@ -26,10 +27,27 @@ export class Rule extends Lint.Rules.TypedRule {
     program: ts.Program
   ): Lint.RuleFailure[] {
     const failures: Lint.RuleFailure[] = [];
+    const typeChecker = program.getTypeChecker();
     const query = `ExpressionStatement[expression.expression.name.text=/^(complete|error)$/] ~ ExpressionStatement[expression.expression.name.text=/^(next|complete|error)$/]`;
     const expressionStatements = tsquery(sourceFile, query);
-    expressionStatements.forEach(node => {
-      const expressionStatement = node as ts.ExpressionStatement;
+    expressionStatements.forEach((node: ts.ExpressionStatement) => {
+      const { parent } = node;
+      if (!ts.isBlock(parent)) {
+        return;
+      }
+      const { statements } = parent;
+      const index = statements.indexOf(node);
+      const sibling = statements[index - 1] as ts.ExpressionStatement;
+      if (
+        !isExpressionObservable(sibling, typeChecker) ||
+        !isExpressionObservable(node, typeChecker)
+      ) {
+        return;
+      }
+      if (getExpressionText(sibling) !== getExpressionText(node)) {
+        return;
+      }
+      const expressionStatement = node;
       if (ts.isCallExpression(expressionStatement.expression)) {
         const callExpression = expressionStatement.expression;
         if (ts.isPropertyAccessExpression(callExpression.expression)) {
@@ -48,4 +66,34 @@ export class Rule extends Lint.Rules.TypedRule {
     });
     return failures;
   }
+}
+
+function getExpressionText(
+  expressionStatement: ts.ExpressionStatement
+): string | undefined {
+  if (!ts.isCallExpression(expressionStatement.expression)) {
+    return undefined;
+  }
+  const callExpression = expressionStatement.expression;
+  if (!ts.isPropertyAccessExpression(callExpression.expression)) {
+    return undefined;
+  }
+  const { expression } = callExpression.expression;
+  return expression.getText();
+}
+
+function isExpressionObservable(
+  expressionStatement: ts.ExpressionStatement,
+  typeChecker: ts.TypeChecker
+): boolean {
+  if (!ts.isCallExpression(expressionStatement.expression)) {
+    return false;
+  }
+  const callExpression = expressionStatement.expression;
+  if (!ts.isPropertyAccessExpression(callExpression.expression)) {
+    return false;
+  }
+  const { expression } = callExpression.expression;
+  const type = typeChecker.getTypeAtLocation(expression);
+  return couldBeType(type, /^(Subject|Subscriber)$/);
 }
