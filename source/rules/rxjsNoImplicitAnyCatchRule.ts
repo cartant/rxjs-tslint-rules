dfe337111d8153c386db60bcb1ce880c54c8da42
Nicholas Jamieson <nicholas@cartant.com>
2020-07-11 14:40:06 +1000

    feat: Add no-implicit-any-catch rule.

---
+++ source/rules/rxjsNoImplicitAnyCatchRule.ts
@@ -0,0 +1,93 @@
+/**
+ * @license Use of this source code is governed by an MIT-style license that
+ * can be found in the LICENSE file at https://github.com/cartant/rxjs-tslint-rules
+ */
+
+import { tsquery } from "@phenomnomnominal/tsquery";
+import * as Lint from "tslint";
+import * as ts from "typescript";
+import * as peer from "../support/peer";
+
+export class Rule extends Lint.Rules.TypedRule {
+  public static metadata: Lint.IRuleMetadata = {
+    deprecationMessage: peer.v5 ? peer.v5NotSupportedMessage : undefined,
+    description:
+      "Disallows implicit `any` error parameters in `catchError` operators.",
+    options: {
+      properties: {
+        allowExplicitAny: { type: "boolean" },
+      },
+      type: "object",
+    },
+    optionsDescription: Lint.Utils.dedent`
+      An optional object with an optional \`allowExplicitAny\` property.`,
+    requiresTypeInfo: true,
+    ruleName: "rxjs-no-implicit-any-catch",
+    type: "functionality",
+    typescriptOnly: true,
+  };
+
+  public static EXPLICIT_ANY = "Explicit any in catchError";
+  public static IMPLICIT_ANY = "Implicit any in catchError";
+
+  public applyWithProgram(
+    sourceFile: ts.SourceFile,
+    program: ts.Program
+  ): Lint.RuleFailure[] {
+    const {
+      ruleArguments: [options],
+    } = this.getOptions();
+    const allowExplicitAny =
+      options && options.hasOwnProperty("allowExplicitAny")
+        ? options.allowExplicitAny
+        : false;
+    const failures: Lint.RuleFailure[] = [];
+
+    const callExpressions = tsquery(
+      sourceFile,
+      `CallExpression[expression.text="catchError"]`
+    ) as ts.CallExpression[];
+
+    callExpressions.forEach((callExpression) => {
+      const [arg] = callExpression.arguments;
+      if (!arg) {
+        return;
+      }
+      if (ts.isArrowFunction(arg) || ts.isFunctionExpression(arg)) {
+        const [parameter] = arg.parameters;
+        if (parameter.type) {
+          if (
+            parameter.type.kind === ts.SyntaxKind.AnyKeyword &&
+            !allowExplicitAny
+          ) {
+            failures.push(
+              new Lint.RuleFailure(
+                sourceFile,
+                parameter.getStart(),
+                parameter.getStart() + parameter.getWidth(),
+                Rule.EXPLICIT_ANY,
+                this.ruleName,
+                Lint.Replacement.replaceNode(parameter.type, "unknown")
+              )
+            );
+          }
+        } else {
+          failures.push(
+            new Lint.RuleFailure(
+              sourceFile,
+              parameter.getStart(),
+              parameter.getStart() + parameter.getWidth(),
+              Rule.IMPLICIT_ANY,
+              this.ruleName,
+              Lint.Replacement.appendText(
+                parameter.getStart() + parameter.getWidth(),
+                ": unknown"
+              )
+            )
+          );
+        }
+      }
+    });
+    return failures;
+  }
+}
