d653cf0c615d54490e164061a1465113c73006ef
Nicholas Jamieson <nicholas@cartant.com>
2020-08-25 16:12:54 +1000

    fix: Allow DOMException.

--- source/rules/rxjsThrowErrorRule.ts
+++ source/rules/rxjsThrowErrorRule.ts
@@ -83,7 +83,7 @@ export class Walker extends Lint.ProgramAwareRuleWalker {
     const typeChecker = this.getTypeChecker();
     const type = typeChecker.getTypeAtLocation(node.expression);
 
-    if (!isAny(type) && !couldBeType(type, "Error")) {
+    if (!isAny(type) && !couldBeType(type, /^(Error|DOMException)$/)) {
       this.addFailureAtNode(node.expression, Rule.FAILURE_STRING);
     }
 
